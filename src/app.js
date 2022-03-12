209be40187f8236c39e0f7c9bce8a68915c87522
Ticklemonster Dad (aka Matt) <mdclements@yahoo.com>
2022-03-12 12:28:47 +0800

    Undo system implemented using COMMAND pattern.

--- src/app.js
+++ src/app.js
@@ -30,7 +30,6 @@ const GAMESTATE_OVER = 'GAME OVER';
 
 let gameState = GAMESTATE_INIT;
 let wins = 0;
-let entities = [];
 
 // Initialize PIXI.js components
 const renderer = new Renderer({ width: 720, height: 720, backgroundColor: 0x00008b });
@@ -55,16 +54,23 @@ ticker.add((t) => {
             timeCounter -= t;
             if (timeCounter < 0 && loader.resources['spritesheet']) {
                 gameState = GAMESTATE_NEW;
+                uiSystem.enableNewGameButton(false);
+                uiSystem.enableUndoButton(false);
             }
             break; 
 
         case GAMESTATE_NEW:
             console.debug('Start a NEW GAME');
-            // clear the entity system and start again...
-            entities = [];
 
-            // add the scoreboard entities then start the layout
-            scoreboardSystem.createEntities(entities);
+            // clear the entity system and start again...
+            clearEntities();
+            clearUndo();
+            uiSystem.enableUndoButton(false);
+
+            // add the scoreboard entities then start the layout (no undo)
+            addEntities(
+                scoreboardSystem.createEntities(), false
+            );
             layoutGenerator.startLayout();
             uiSystem.enableNewGameButton(false);
             gameState = GAMESTATE_LAYOUT;
@@ -82,17 +88,17 @@ ticker.add((t) => {
                 gameState = GAMESTATE_RUNNING;
             }
             else if (nextEntity !== undefined) {
-                entities.push(nextEntity);
+                addEntities([nextEntity], false);
             }
 
-            gameSystem.update(t, entities); //.filter(e => e.components.has('piece')));
+            gameSystem.update(t, entities);
             scoreboardSystem.update(t, entities);
             renderSystem.update(t, entities);
 
             break;
 
         case GAMESTATE_RUNNING:
-            gameSystem.update(t, entities); //entities.filter(e => e.components.has('piece')));
+            gameSystem.update(t, entities);
             scoreboardSystem.update(t, entities);
             renderSystem.update(t, entities);
 
@@ -152,18 +158,174 @@ ticker.add((t) => {
 
     // subscribe to game level events
     uiSystem.on('NEW GAME', () => {
-        // console.debug('NEW GAME event received');
+        console.debug('NEW GAME event received');
         gameState = GAMESTATE_NEW;
+    });
+    uiSystem.on('UNDO', () => {
+        console.log('UNDO event from UI');
+        clearSelections();
+        undoAction();
+        if (undoList.length == 0) uiSystem.enableUndoButton(false);
+    });
+    uiSystem.on('REDO', () => {
+        console.log('REDO event from UI');
+        clearSelections();
+        redoAction();
+        // if (redoList.length == 0) uiSystem.enableRedoButton(false);
     });
 
     renderSystem.on('SELECT', (entity) => {
-        console.debug(`SELECT EVENT for entity `, entity);
-        gameSystem.toggleSelect(entity, entities);
-    })
+        // console.debug('SELECT EVENT for ', entity);
+        selectEntities([entity]);
+    });
+    
+    gameSystem.on('UNSELECT', (entities) => {
+        // console.debug(`UNSELECT EVENT for `, entities);
+        selectEntities(entities, false);
+    });
 
+    gameSystem.on('REMOVE', (entities) => {
+        // console.debug('REMOVE EVENT for ', entities);
+        // use the COMMAND to make this undoable
+        selectEntities(entities, false);
+        doAction( () => removeEntities(entities), () => addEntities(entities) );
+    });
+        
 
     // set up the win tracker
     wins = parseInt(window.localStorage.getItem(LOCALSTORAGE_WIN_KEY), 10) || 0;
     uiSystem.setWins( wins );
 
 })();
+
+
+//
+// Command-pattern for user actions to enable undo/redo
+//
+let undoList = [];
+let redoList = [];
+
+function clearUndo() {
+    undoList = [];
+    redoList = [];
+}
+
+function doAction(action, undoaction) {
+    action();
+    undoList.push({ 'action': action, 'undoaction': undoaction });
+    redoList = [];
+    
+    uiSystem.enableUndoButton(true);
+}
+
+function undoAction() {
+    const command = undoList.pop();
+    if (command) {
+        command.undoaction();
+        redoList.push(command);
+    }
+
+    uiSystem.enableUndoButton(undoList.length > 0);
+    // uiSystem.enableRedoButton(redoList.length > 0);
+}
+
+function redoAction() {
+    const command = redoList.pop();
+    if (command) {
+        command.action();
+        undoList.push(command)
+    }
+}
+
+//
+// Entity Management
+//
+let entities = [];
+
+function clearEntities() {
+    entities = [];
+}
+
+function addEntities(es) {
+    es.forEach(e => console.debug(`[ add entity ${e.name}]`));
+    entities = [ ...entities, ...es ];
+}
+
+function removeEntities(es) {
+    // "destroy" each component
+    for (let e of es) {
+        console.debug(`[ remove entity ${e.name}]`);
+        for (let c of e.components) {
+            if (c.destroy && typeof c.destroy == 'function') c.destroy();
+        }
+    }
+
+    // remove the entities
+    entities = entities.filter(e => !es.includes(e));
+}
+
+
+//
+// select or unselect an entity
+// if not defined, then toggle the current state
+//
+function selectEntities(ens, shouldSelect = undefined) {
+    for (let entity of ens) {
+        if (entity && entity.components.has('piece')) {
+            const p = entity.components.get('piece');
+            p.selected = (shouldSelect === undefined) ? !p.selected : shouldSelect;
+
+            if (p.selected) {
+                console.debug(`[ select piece ${entity.name}]`);
+                addEntities([newSelectorEntityFor(entity)]);
+            } else {
+                console.debug(`[ unselect piece ${entity.name}]`);
+                const selectors = entities.filter(e => e.components.has('selector') && e.components.get('selector').entity == entity);
+                removeEntities(selectors);
+            }
+        }
+    }
+
+}
+
+function clearSelections() {
+    const selected = entities.filter(e => e.components.has('piece') && e.components.get('piece').selected);
+    selectEntities(selected, false);
+}
+
+function newSelectorEntityFor(other) {
+    const entity = {
+        name: 'selector for ' + other.name,
+        components: new Map([
+            ['selector', {
+                'entity': other,
+            }],
+        ])
+    }
+
+    return entity;
+}
+
+// function destroyEntityComponents(entity) {
+//     entity.components.forEach(c => {
+//         if (c.destroy && typeof(c.destroy) === 'function') {
+//             c.destroy();
+//         }
+//     })
+// }
+// 
+// function removeSelectorFrom(entity, entities) {
+//     let i = -1;
+//     while ((i = entities.findIndex(e => e.components.has('selector') && e.components.get('selector').entity == entity)) > -1) {
+//         destroyEntityComponents(entities[i]);
+//         entities.splice(i, 1);
+//     }
+// }
+
+// function removeEntityFrom(entity, entities) {
+//     const i = entities.findIndex(e => e == entity);
+//     if (i > -1) {
+//         destroyEntityComponents(entity);
+//         entities.splice(i, 1);
+//     }
+// }
